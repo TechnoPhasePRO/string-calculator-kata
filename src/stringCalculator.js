7407063c4edfd6cba7c866d28fade716f134e584
TechnoPhasePRO <yrajsingh0001@gmail.com>
2024-06-30 12:50:35 +0530

    Handle new lines between numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,8 @@
 function add(numbers) {
   if (numbers === '') return 0;
-  if (!numbers.includes(',')) return parseInt(numbers);
+  numbers = numbers.replace(/\n/g, ',');
   const nums = numbers.split(',').map(Number);
   return nums.reduce((a, b) => a + b, 0);
 }
 
-module.exports = { add };
-
-  
+module.exports = { add };  
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,6 +1,7 @@
 const { add } = require('../src/stringCalculator');
 
-test('should return the sum of multiple comma-separated numbers', () => {
-    expect(add('1,2,3,4')).toBe(10);
-}); 
+test('should handle new lines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
+});
+  
   
