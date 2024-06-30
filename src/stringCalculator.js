bd7798746582e1bc20d8c523608d2cddbbb0b3cb
TechnoPhasePRO <yrajsingh0001@gmail.com>
2024-06-30 12:55:28 +0530

    Support different delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,16 @@
 function add(numbers) {
   if (numbers === '') return 0;
-  numbers = numbers.replace(/\n/g, ',');
-  const nums = numbers.split(',').map(Number);
+
+  let delimiter = ',';
+  if (numbers.startsWith('//')) {
+    const delimiterLineEnd = numbers.indexOf('\n');
+    delimiter = numbers.substring(2, delimiterLineEnd);
+    numbers = numbers.substring(delimiterLineEnd + 1);
+  }
+
+  numbers = numbers.replace(/\n/g, delimiter);
+  const nums = numbers.split(delimiter).map(Number);
   return nums.reduce((a, b) => a + b, 0);
 }
 
-module.exports = { add };  
+module.exports = { add };
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,7 +1,8 @@
 const { add } = require('../src/stringCalculator');
 
-test('should handle new lines between numbers', () => {
-    expect(add('1\n2,3')).toBe(6);
+test('should support different delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3);
 });
+  
   
   
